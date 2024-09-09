6affa6d151d75c79cc14a10632d7c0f096a60e85
Davidbot1 <169171650+Davidbot1@users.noreply.github.com>
2024-09-10 00:49:29 +0100

    Create Merci.js

---
+++ scripts/cmds/Merci.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "merci",
+        version: "1.0",
+        author: "kivv",
+        countDown: 5,
+        role: 0,
+        shortDescription: "Nppo Prefix",
+        longDescription: "Nppo Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "Merci") return message.reply(" pas de quoi 🦅? je suis là pour répondre à tout tes question s ✨🌿✅");
+}
+};
