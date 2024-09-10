7477effe3638a21d9e4192752d9ba9738ecb5e16
Davidbot1 <169171650+Davidbot1@users.noreply.github.com>
2024-09-10 12:44:01 +0100

    Create B.js

---
+++ scripts/cmds/B.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "🦅",
+        version: "1.0",
+        author: "𝐦𝐚𝐝𝐚𝐫𝐚",
+        countDown: 5,
+        role: 0,
+        shortDescription: "commande 🦅😐",
+        longDescription: "commande O🦅k",
+        category: "reply",
+    },
+    onStart: async function(){}, 
+    onChat: async function({
+        event,
+        message,
+        getLang
+    }) {
+        if (event.body && event.body.toLowerCase() == "🦅") return message.reply("David mpongo ☝️___________◥✇◣, ,◢✇◤_________ce lui le meilleur ☝️_");
+    }
+}
